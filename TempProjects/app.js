a1ce7f1c9417e530ad8d5c9df66c2ff794bb765d
switch2512 <92006458+switch2512@users.noreply.github.com>
2022-01-10 11:19:45 -0800

    Update app.js

--- TempProjects/app.js
+++ TempProjects/app.js
@@ -1,8 +1,14 @@
-let fs = require('fs');
-let exampleJSON = {};
-fs.readFile('test.json', 'utf8', function(err, data) {
-    exampleJSON = JSON.parse(data);
-})
-
-console.log(exampleJSON)
-console.log("this is the testing thing:" + exampleJSON.person.name)
+let test = {
+    field = [
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0]
+        ]
+}
